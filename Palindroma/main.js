6b03de212fc76ce4171563e2d444939b83746d11
Brina <brinamotto@gmail.com>
2024-05-20 22:34:49 +0200

    [add] palindroma

---
+++ Palindroma/main.js
@@ -0,0 +1,19 @@
+
+let userInput = prompt('Inserisci una parola');
+
+
+
+function palindrome(word) {
+    let reverseWord = word.split('').reverse().join('');
+    return word === reverseWord
+};
+
+if (palindrome(userInput)) {
+    console.log('Questa è una parola palindroma');
+} else {
+    console.log('Questa non è una parola palindroma')
+};
+
+
+
+
